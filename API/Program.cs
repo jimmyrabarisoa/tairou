1822c169df5de2a812c82c26d01fe6530001964f
jimmyrabarisoa <jimmy.rabarisoa@gmail.com>
2026-03-31 17:33:18 +0300

    configuration CORS

--- API/Program.cs
+++ API/Program.cs
@@ -9,8 +9,17 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); 
 });
+builder.Services.AddCors(); // permet à l'application backend de gérer des requêtes provenant de différents domaines (origins).
+// Sans ça, le backend refuserait par défaut les requêtes provenant d’un autre domaine ou port que lui-même (par exemple, le frontend React qui tourne sur localhost:3000).
 
 var app = builder.Build();
+
+// var définir la politique CORS que l'application va appliquer aux requêtes HTTP entrantes.
+app.UseCors(x => 
+	x.AllowAnyHeader() // Autorise tous les headers HTTP
+	.AllowAnyMethod()  // Autorise toutes les méthodes HTTP (GET, POST, PUT, DELETE…)
+	.WithOrigins("https://localhost:7089") // Autorise seulement ces origines
+);
 
 app.MapControllers();
 
