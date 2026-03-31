3a71aa3b90476baafdb22931bdf30013c1ca84a8
jimmyrabarisoa <jimmy.rabarisoa@gmail.com>
2026-03-31 17:22:08 +0300

    skeleton ideas API

---
+++ API/Controllers/IdeasController.cs
@@ -0,0 +1,30 @@
+using System;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers;
+
+public class IdeasController(AppDbContext context) : BaseAPIController
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Idea>>> GetIdeas()
+    {
+        return await context.Ideas.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Idea>> GetIdea(string id)
+    {
+        return await context.Ideas.FindAsync(id) ?? throw new Exception("Idea not found.");
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<string>> CreateIdea(Idea idea)
+    {
+        context.Ideas.Add(idea);
+        await context.SaveChangesAsync();
+        return idea.Id;
+    }
+}
