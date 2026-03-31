bfc626e94bc3d3d0a302a166902e54aa420968ee
jimmyrabarisoa <jimmy.rabarisoa@gmail.com>
2026-03-31 18:17:41 +0300

    ui topbar

--- client/src/layouts/App.jsx
+++ client/src/layouts/App.jsx
@@ -1,11 +1,12 @@
 import Navbar from "../components/Navbar"
 import { Outlet } from "react-router"
+import Topbar from "../components/Topbar"
 
 function App() {
 
   return (
     <div className="flex flex-col mx-auto min-h-dvh max-w-200 p-5">
-      {/* <Topbar /> */}
+      <Topbar />
       <div className="flex-1">
         <Outlet />  
       </div>   
